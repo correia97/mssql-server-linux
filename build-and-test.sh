3c835228fd3dc52a56c1c841f962ecce7351a61f
Paulo Eduardo Correia <paulo.correia@docway.co>
2020-07-29 23:54:04 -0300

    ajuste para manter compatiilidade

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,6 +1,7 @@
 clear 
+echo 'Build do 2017'
 
-docker build . --no-cache -t luizcarlosfaria/mssql-server-linux:latest
+docker build . --no-cache -t luizcarlosfaria/mssql-server-linux:2017-latest -f Dockerfile-sql2017
 
 docker run \
 --rm \
@@ -12,4 +13,22 @@ docker run \
 -e 'MSSQL_USER=bbb' \
 -e 'MSSQL_PASSWORD=ccc' \
 -p 1433:1433 \
-luizcarlosfaria/mssql-server-linux:latest
+luizcarlosfaria/mssql-server-linux:2017-latest
+
+
+clear
+echo 'Build do 2019'
+
+docker build . --no-cache -t luizcarlosfaria/mssql-server-linux:2019-latest -f Dockerfile-sql2019
+
+docker run \
+--rm \
+-it \
+-e 'ACCEPT_EULA=Y' \
+-e 'SA_PASSWORD=8k3W2C2BVzAW9p' \
+-e 'MSSQL_DATABASE=db01' \
+-e 'MSSQL_DATABASE_COLLATE=SQL_Latin1_General_CP1_CI_AI' \
+-e 'MSSQL_USER=bbb' \
+-e 'MSSQL_PASSWORD=ccc' \
+-p 1434:1433 \
+luizcarlosfaria/mssql-server-linux:2019-latest
